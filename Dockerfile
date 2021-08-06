fb84c78f256c4ce353fd522916bd9b719a7fe3a7
Aleksandr Aleksieienko <wdmaster@gmail.com>
2021-08-06 09:16:49 +0200

    first commit of MaxScale & MariaDB Galera cluster

---
+++ Dockerfile
@@ -0,0 +1,41 @@
+FROM mariadb/maxscale:2.5.14
+MAINTAINER wdmaster@gmail.com
+
+# Setup for Galera Service (GS), not for Master-Slave environments
+
+# We set some defaults for config creation. Can be overwritten at runtime.
+ENV MAX_USER="maxscale" \
+    MAX_PASS="maxscalepass" \
+    ENABLE_ROOT_USER=0 \ 
+    SPLITTER_PORT=3306 \
+    ROUTER_PORT=3307 \
+    API_PORT=8989 \
+    CONNECTION_TIMEOUT=600 \
+    PERSIST_POOLMAX=0 \
+    PERSIST_MAXTIME=3600s \
+    BACKEND_SERVER_LIST="server1 server2 server3" \
+    BACKEND_SERVER_PORT="3306" \
+    USE_SQL_VARIABLES_IN="all"
+
+# Move configuration file in directory for exports
+RUN mkdir -p /etc/maxscale.d \
+    && cp /etc/maxscale.cnf.template /etc/maxscale.d/maxscale.cnf \
+    && ln -sf /etc/maxscale.d/maxscale.cnf /etc/maxscale.cnf \
+    && chown root:maxscale /etc/maxscale.d/maxscale.cnf \
+    && chmod g+w /etc/maxscale.d/maxscale.cnf
+
+# VOLUME for custom configuration
+VOLUME ["/etc/maxscale.d"]
+
+# We expose our set Listener Ports
+EXPOSE $SPLITTER_PORT $ROUTER_PORT $API_PORT
+
+COPY maxscale.cnf /etc/
+ENTRYPOINT ["maxscale", "-d", "-U", "maxscale", "-l", "stdout"]
+
+COPY docker-entrypoint.sh /docker-entrypoint-mxs.sh
+RUN chmod 755 /docker-entrypoint-mxs.sh
+
+ENTRYPOINT ["/docker-entrypoint-mxs.sh"]
+
+CMD ["maxscale", "-d", "-U", "maxscale", "-l", "stdout"]
---
+++ docker-entrypoint.sh
@@ -0,0 +1,81 @@
+#!/bin/bash
+
+set -e
+
+# if service discovery was activated, we overwrite the BACKEND_SERVER_LIST with the
+# results of DNS service lookup
+if [ -n "$DB_SERVICE_NAME" ]; then
+  BACKEND_SERVER_LIST=`getent hosts tasks.$DB_SERVICE_NAME|awk '{print $1}'|tr '\n' ' '`
+fi
+
+# We break our IP list into array
+IFS=', ' read -r -a backend_servers <<< "$BACKEND_SERVER_LIST"
+
+
+config_file="/etc/maxscale.cnf"
+
+# We start config file creation
+
+cat <<EOF > $config_file
+[maxscale]
+admin_secure_gui=false
+threads=auto
+admin_host=0.0.0.0
+
+[Galera-Service]
+type=service
+router=readconnroute
+router_options=synced
+targets=${BACKEND_SERVER_LIST// /,}
+connection_timeout=$CONNECTION_TIMEOUT
+user=$MAX_USER
+password=$MAX_PASS
+enable_root_user=$ENABLE_ROOT_USER
+
+[Galera-Listener]
+type=listener
+service=Galera-Service
+protocol=mariadbclient
+port=$ROUTER_PORT
+
+[Splitter-Service]
+type=service
+router=readwritesplit
+targets=${BACKEND_SERVER_LIST// /,}
+connection_timeout=$CONNECTION_TIMEOUT
+user=$MAX_USER
+password=$MAX_PASS
+enable_root_user=$ENABLE_ROOT_USER
+use_sql_variables_in=$USE_SQL_VARIABLES_IN
+
+[Splitter-Listener]
+type=listener
+service=Splitter-Service
+protocol=mariadbclient
+port=$SPLITTER_PORT
+
+[Galera-Monitor]
+type=monitor
+module=galeramon
+servers=${BACKEND_SERVER_LIST// /,}
+disable_master_failback=false
+user=$MAX_USER
+password=$MAX_PASS
+# Start the Server block
+EOF
+
+# add the [server] block
+for i in ${!backend_servers[@]}; do
+cat <<EOF >> $config_file
+[${backend_servers[$i]}]
+type=server
+address=${backend_servers[$i]}
+port=$BACKEND_SERVER_PORT
+persistpoolmax=$PERSIST_POOLMAX
+persistmaxtime=$PERSIST_MAXTIME
+EOF
+
+done
+
+
+exec "$@"
